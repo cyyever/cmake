0e4afba219e2938eeeb1ef1fed310890543e7034
cyy <cyyever@outlook.com>
2018-03-26 17:15:13 +0800

    add test code

---
+++ test.cmake
@@ -0,0 +1,8 @@
+#apt-get install extra-cmake-modules
+FIND_PACKAGE(ECM REQUIRED)
+LIST(APPEND CMAKE_MODULE_PATH "${ECM_MODULE_DIR}")
+
+#給測試增加llvm sanitizer
+include(ECMEnableSanitizers)
+#set(ECM_ENABLE_SANITIZERS 'address;leak;undefined')
+set(ECM_ENABLE_SANITIZERS 'leak;undefined')
