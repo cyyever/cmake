4adeb0c93f3db8e2b9e81fd3c183b68dc58b24cc
cyy <cyyever@outlook.com>
2018-03-27 15:50:53 +0800

    only in linux

--- test.cmake
+++ test.cmake
@@ -1,10 +1,12 @@
 INCLUDE(${CMAKE_CURRENT_LIST_DIR}/compiler.cmake)
 
-#apt-get install extra-cmake-modules
-FIND_PACKAGE(ECM REQUIRED)
-LIST(APPEND CMAKE_MODULE_PATH "${ECM_MODULE_DIR}")
+IF(${CMAKE_HOST_SYSTEM_NAME} EQUAL "Linux")
+  #apt-get install extra-cmake-modules
+  FIND_PACKAGE(ECM REQUIRED)
+  LIST(APPEND CMAKE_MODULE_PATH "${ECM_MODULE_DIR}")
 
-#給測試增加llvm sanitizer
-include(ECMEnableSanitizers)
-#set(ECM_ENABLE_SANITIZERS 'address;leak;undefined')
-set(ECM_ENABLE_SANITIZERS 'undefined;leak')
+  #給測試增加llvm sanitizer
+  include(ECMEnableSanitizers)
+  #set(ECM_ENABLE_SANITIZERS 'address;leak;undefined')
+  set(ECM_ENABLE_SANITIZERS 'undefined;leak')
+ENDIF()
