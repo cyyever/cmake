3f7a596dccbc55a9ea9a6e68b7e483a4cbf8256d
cyy <cyyever@outlook.com>
2018-03-29 17:59:31 +0800

    add valgrind support

--- test.cmake
+++ test.cmake
@@ -1,12 +1,31 @@
 INCLUDE(${CMAKE_CURRENT_LIST_DIR}/compiler.cmake)
 
-IF(${CMAKE_HOST_SYSTEM_NAME} EQUAL "Linux")
-  #apt-get install extra-cmake-modules
-  FIND_PACKAGE(ECM REQUIRED)
-  LIST(APPEND CMAKE_MODULE_PATH "${ECM_MODULE_DIR}")
+#IF(${CMAKE_HOST_SYSTEM_NAME} EQUAL "Linux")
+#  #apt-get install extra-cmake-modules
+#  FIND_PACKAGE(ECM REQUIRED)
+#  LIST(APPEND CMAKE_MODULE_PATH "${ECM_MODULE_DIR}")
+#
+#  #給測試增加llvm sanitizer
+#  include(ECMEnableSanitizers)
+#  #set(ECM_ENABLE_SANITIZERS 'address;leak;undefined')
+#  set(ECM_ENABLE_SANITIZERS 'undefined;leak')
+#ENDIF()
 
-  #給測試增加llvm sanitizer
-  include(ECMEnableSanitizers)
-  #set(ECM_ENABLE_SANITIZERS 'address;leak;undefined')
-  set(ECM_ENABLE_SANITIZERS 'undefined;leak')
-ENDIF()
+
+include(FindPackageHandleStandardArgs)
+find_path(VALGRIND_PATH valgrind PATHS /usr/bin /usr/local/bin)
+find_package_handle_standard_args(valgrind VALGRIND_PATH)
+
+FILE(GLOB suppression_files ${CMAKE_CURRENT_LIST_DIR}/*.supp)
+
+set(memcheck_command "${VALGRIND_PATH}/valgrind --error-exitcode=1 --trace-children=yes --gen-suppressions=all --track-fds=yes --leak-check=full")
+foreach(suppression_file ${suppression_files})
+  set(memcheck_command "${memcheck_command} --suppressions=${suppression_file}")
+endforeach()
+
+function(add_test_with_runtime_analysis name binary)
+  if(valgrind_FOUND)
+    separate_arguments(memcheck_command)
+    add_test(NAME memcheck_${name} COMMAND ${memcheck_command} ./${binary} ${ARGN})
+  endif()
+endfunction(add_test_with_runtime_analysis)
