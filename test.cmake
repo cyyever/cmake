49b2e5cd63a94f89bfa77ee6d5274c51d5b26e63
cyy <cyyever@outlook.com>
2018-04-14 19:56:44 +0800

    add freebsd find support

--- test.cmake
+++ test.cmake
@@ -44,6 +44,6 @@ endfunction(add_test_with_runtime_analysis)
 FIND_PACKAGE(gcovr)
 if(ENABLE_CODE_COVERAGE AND NOT TARGET code_coverage AND gcovr_FOUND)
   ADD_CUSTOM_TARGET(code_coverage ALL 
-    COMMAND mkdir -p ${CMAKE_BINARY_DIR}/code_coverage && ${gcovr_BINARY} -r ${CMAKE_SOURCE_DIR} --object-directory=`find ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}  -name '*.gcno' -printf '%h\\n'  -quit` --html --html-details -o ${CMAKE_BINARY_DIR}/code_coverage/index.html
+    COMMAND mkdir -p ${CMAKE_BINARY_DIR}/code_coverage && ${gcovr_BINARY} -r ${CMAKE_SOURCE_DIR} --object-directory=`find ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY} -name '*.gcno' -print0 -quit | xargs -0 -n 1 dirname ` --html --html-details -o ${CMAKE_BINARY_DIR}/code_coverage/index.html
     DEPENDS check)
 endif()
