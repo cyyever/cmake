45ca67cdd8e6ea7dca328223337dd1ee24bcb243
cyy <cyyever@outlook.com>
2018-04-23 11:56:05 +0800

    fix doxygen

--- documentation.cmake
+++ documentation.cmake
@@ -2,4 +2,6 @@ LIST(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR}/module)
 
 FIND_PACKAGE(doxygen)
 
-ADD_CUSTOM_TARGET(doc COMMAND ${doxygen_BINARY} -g && sed -i -e "s@My Project@${CMAKE_PROJECT_NAME}@" Doxyfile && sed -i -e "s@^INPUT[[:space:]]*=.*@INPUT = ${CMAKE_CURRENT_LIST_DIR}/src@" Doxyfile && ${doxygen_BINARY})
+IF(doxygen_FOUND)
+  ADD_CUSTOM_TARGET(doc COMMAND ${doxygen_BINARY} -g && sed -i -e "s@My Project@${CMAKE_PROJECT_NAME}@" Doxyfile && sed -i -e "s@^INPUT[[:space:]]*=.*@INPUT = ${CMAKE_SOURCE_DIR}/src@" Doxyfile && ${doxygen_BINARY})
+endif()
