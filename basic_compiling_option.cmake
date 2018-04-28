d7d531789d62d10ef468bc0dbb8b0920fcc8f656
cyy <cyyever@outlook.com>
2018-04-28 12:01:39 +0800

    enable some compiling options and rename cmake file

--- basic_compiling_option.cmake
+++ basic_compiling_option.cmake
@@ -5,7 +5,7 @@ if(NOT CMAKE_BUILD_TYPE)
 endif()
 
 IF(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-  add_compile_options("-Weverything" "-Wno-c++98-compat" "-Wno-zero-as-null-pointer-constant" "-Wno-c++98-compat-pedantic" "-Wno-padded" "-Wno-double-promotion" "-Wno-weak-vtables" "-Wno-disabled-macro-expansion" "-Wno-reserved-id-macro" "-Wno-global-constructors" "-Wno-exit-time-destructors")
+  add_compile_options("-Weverything" "-Wno-c++98-compat" "-Wno-c++98-compat-pedantic" "-Wno-padded" "-Wno-weak-vtables" "-Wno-disabled-macro-expansion" "-Wno-reserved-id-macro" "-Wno-global-constructors" "-Wno-exit-time-destructors")
 ELSEIF(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
   add_compile_options("-Wall" "-Wextra" "-Wshadow" "-Wnon-virtual-dtor" "-pedantic")
 ELSEIF(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
