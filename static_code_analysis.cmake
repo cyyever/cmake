a47aada55b5ed72d3eb07bb4d5ce4fc114de8548
cyy <cyyever@outlook.com>
2018-04-28 11:58:14 +0800

    refactor some code and add thread analysis

--- static_code_analysis.cmake
+++ static_code_analysis.cmake
@@ -1,4 +1,9 @@
-#static code analysis
+IF(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+  add_compile_options("/analyze")
+ELSEIF(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+  add_compile_options("-Wthread-safety")
+ENDIF()
+
 SET(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
 LIST(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR}/module)
@@ -9,22 +14,16 @@ if(clangtidy_FOUND)
 ENDIF()
 
 FIND_PACKAGE(cppcheck)
-if(cppcheck_FOUND)
+if(NOT TARGET do_cppcheck AND cppcheck_FOUND)
   add_custom_target(do_cppcheck ALL
     COMMAND ${cppcheck_BINARY} --project=${CMAKE_BINARY_DIR}/compile_commands.json --std=c++14 --enable=all
     DEPENDS ${CMAKE_BINARY_DIR}/compile_commands.json)
 endif()
 
-IF(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
-  add_compile_options("/analyze")
-ENDIF()
-
-IF (NOT WIN32)
-  FIND_PACKAGE(scanbuild)
-  if(NOT TARGET scan_build AND scanbuild_FOUND)
-    ADD_CUSTOM_TARGET(scan_build
-      COMMAND cmake -DCMAKE_CXX_COMPILER=${cpp_analyzer_BINARY} -DCMAKE_C_COMPILER=${ccc_analyzer_BINARY} ${CMAKE_BINARY_DIR}
-      COMMAND ${scanbuild_BINARY} ${CMAKE_COMMAND} --build ${CMAKE_BINARY_DIR}
-      )
-  endif()
+FIND_PACKAGE(scanbuild)
+if(NOT TARGET scan_build AND scanbuild_FOUND)
+  ADD_CUSTOM_TARGET(scan_build
+    COMMAND cmake -DCMAKE_CXX_COMPILER=${cpp_analyzer_BINARY} -DCMAKE_C_COMPILER=${ccc_analyzer_BINARY} ${CMAKE_BINARY_DIR}
+    COMMAND ${scanbuild_BINARY} ${CMAKE_COMMAND} --build ${CMAKE_BINARY_DIR}
+    )
 endif()
