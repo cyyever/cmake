e6b025014a88b9247ee2b119523cf98f431f6ecb
cyy <cyyever@outlook.com>
2018-04-29 19:55:31 +0800

    add windows support

--- module/Findcppcheck.cmake
+++ module/Findcppcheck.cmake
@@ -5,9 +5,19 @@
 #  cppcheck_BINARY
 
 include(FindPackageHandleStandardArgs)
-find_path(cppcheck_DIR cppcheck PATHS /usr/bin /usr/local/bin)
+
+if(WIN32)
+  set(_PF86 "ProgramFiles(x86)")
+  find_path(cppcheck_DIR cppcheck.exe PATHS "$ENV{PROGRAMFILES}/Cppcheck" "$ENV{${_PF86}}/Cppcheck" "$ENV{ProgramW6432}/Cppcheck"  )
+else()
+  find_path(cppcheck_DIR cppcheck PATHS /usr/bin /usr/local/bin)
+endif()
 find_package_handle_standard_args(cppcheck DEFAULT_MSG cppcheck_DIR)
 
 if(cppcheck_FOUND)
-  set(cppcheck_BINARY "${cppcheck_DIR}/cppcheck")
+  if(WIN32)
+    set(cppcheck_BINARY "${cppcheck_DIR}/cppcheck.exe")
+  else()
+    set(cppcheck_BINARY "${cppcheck_DIR}/cppcheck")
+  endif()
 endif()
