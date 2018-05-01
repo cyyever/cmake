369b88502a13a5a3b6f240c0f8f2c7076d6eff41
cyy <cyyever@outlook.com>
2018-05-01 11:10:04 +0800

    clean up windows option

--- all.cmake
+++ all.cmake
@@ -1,5 +1,6 @@
 INCLUDE(${CMAKE_CURRENT_LIST_DIR}/basic_compiling_option.cmake)
 INCLUDE(${CMAKE_CURRENT_LIST_DIR}/linker.cmake)
+INCLUDE(${CMAKE_CURRENT_LIST_DIR}/optimization.cmake)
 INCLUDE(${CMAKE_CURRENT_LIST_DIR}/static_code_analysis.cmake)
 INCLUDE(${CMAKE_CURRENT_LIST_DIR}/windows.cmake)
 INCLUDE(${CMAKE_CURRENT_LIST_DIR}/code_coverage.cmake)
--- linker.cmake
+++ linker.cmake
@@ -1,9 +1,1 @@
-include(CheckIPOSupported)
-check_ipo_supported(RESULT result OUTPUT output LANGUAGES C CXX)
-if(result)
-  set(CMAKE_INTERPROCEDURAL_OPTIMIZATION TRUE)
-else()
-  message(WARNING "IPO is not supported: ${output}")
-endif()
-
 SET(CMAKE_LINK_WHAT_YOU_USE TRUE)
