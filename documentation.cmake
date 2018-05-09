a52e25b6c276403027093883fd896e2c6ba9d567
cyy <cyyever@outlook.com>
2018-05-09 15:30:11 +0800

    avid readd target

--- documentation.cmake
+++ documentation.cmake
@@ -2,6 +2,6 @@ LIST(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR}/module)
 
 FIND_PACKAGE(doxygen)
 
-IF(doxygen_FOUND)
+IF(doxygen_FOUND AND NOT TARGET doc)
   ADD_CUSTOM_TARGET(doc COMMAND ${doxygen_BINARY} -g && sed -i -e "s@My Project@${CMAKE_PROJECT_NAME}@" Doxyfile && sed -i -e "s@^INPUT[[:space:]]*=.*@INPUT = ${CMAKE_SOURCE_DIR}/src@" Doxyfile && ${doxygen_BINARY})
 endif()
