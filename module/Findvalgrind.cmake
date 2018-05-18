b398618c864dc42aa33bfb6dfceaf35b74e1274c
cyy <cyyever@outlook.com>
2018-05-18 23:36:09 +0800

    use find_program

--- module/Findvalgrind.cmake
+++ module/Findvalgrind.cmake
@@ -5,9 +5,5 @@
 #  valgrind_BINARY
 
 include(FindPackageHandleStandardArgs)
-find_path(VALGRIND_DIR valgrind PATHS /usr/bin /usr/local/bin)
-find_package_handle_standard_args(valgrind DEFAULT_MSG VALGRIND_DIR)
-
-if(valgrind_FOUND)
-  set(valgrind_BINARY "${VALGRIND_DIR}/valgrind")
-endif()
+find_program(valgrind_BINARY valgrind PATHS /usr/bin /usr/local/bin)
+find_package_handle_standard_args(valgrind DEFAULT_MSG valgrind_BINARY)
