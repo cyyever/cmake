27bc196ec749600ebf5043508b636ecf42133029
cyy <cyyever@outlook.com>
2018-05-18 11:22:20 +0800

    use find_program

--- module/Findcppcheck.cmake
+++ module/Findcppcheck.cmake
@@ -6,18 +6,6 @@
 
 include(FindPackageHandleStandardArgs)
 
-if(WIN32)
-  set(_PF86 "ProgramFiles(x86)")
-  find_path(cppcheck_DIR cppcheck.exe PATHS "$ENV{PROGRAMFILES}/Cppcheck" "$ENV{${_PF86}}/Cppcheck" "$ENV{ProgramW6432}/Cppcheck")
-else()
-  find_path(cppcheck_DIR cppcheck PATHS /usr/bin /usr/local/bin)
-endif()
-find_package_handle_standard_args(cppcheck DEFAULT_MSG cppcheck_DIR)
-
-if(cppcheck_FOUND)
-  if(WIN32)
-    set(cppcheck_BINARY "${cppcheck_DIR}/cppcheck.exe")
-  else()
-    set(cppcheck_BINARY "${cppcheck_DIR}/cppcheck")
-  endif()
-endif()
+set(_PF86 "ProgramFiles(x86)")
+find_program(cppcheck_BINARY cppcheck PATHS "$ENV{PROGRAMFILES}/Cppcheck" "$ENV{${_PF86}}/Cppcheck" "$ENV{ProgramW6432}/Cppcheck")
+find_package_handle_standard_args(cppcheck DEFAULT_MSG cppcheck_BINARY)
