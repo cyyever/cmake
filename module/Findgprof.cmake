e2255e1accf9f11f726c0841554b633aa176a81b
cyy <cyyever@outlook.com>
2018-05-18 23:34:44 +0800

    use find_program

--- module/Findgprof.cmake
+++ module/Findgprof.cmake
@@ -3,12 +3,7 @@
 # The following are set after configuration is done:
 #  gprof_FOUND
 #  gprof_BINARY
-#  genhtml_BINARY
 
 include(FindPackageHandleStandardArgs)
-find_path(gprof_DIR gprof PATHS /usr/bin /usr/local/bin)
-find_package_handle_standard_args(gprof DEFAULT_MSG gprof_DIR)
-
-if(gprof_FOUND)
-  set(gprof_BINARY "${gprof_DIR}/gprof")
-endif()
+find_program(gprof_BINARY gprof PATHS /usr/bin /usr/local/bin)
+find_package_handle_standard_args(gprof DEFAULT_MSG gprof_BINARY)
--- module/Findlcov.cmake
+++ module/Findlcov.cmake
@@ -6,10 +6,6 @@
 #  genhtml_BINARY
 
 include(FindPackageHandleStandardArgs)
-find_path(lcov_DIR lcov PATHS /usr/bin /usr/local/bin)
-find_package_handle_standard_args(lcov DEFAULT_MSG lcov_DIR)
-
-if(lcov_FOUND)
-  set(lcov_BINARY "${lcov_DIR}/lcov")
-  set(genhtml_BINARY "${lcov_DIR}/genhtml")
-endif()
+find_program(lcov_BINARY lcov PATHS /usr/bin /usr/local/bin)
+find_program(genhtml_BINARY genhtml PATHS /usr/bin /usr/local/bin)
+find_package_handle_standard_args(lcov DEFAULT_MSG lcov_BINARY genhtml_BINARY)
