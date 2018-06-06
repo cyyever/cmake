ed22cfdbfcbd4fbedffb83956c8ad97926e4f1c9
cyy <cyyever@outlook.com>
2018-06-06 09:12:13 +0000

    fix path find

--- module/FindTensorFlow.cmake
+++ module/FindTensorFlow.cmake
@@ -14,18 +14,28 @@ set(TensorFlow_DIR "" CACHE PATH "Folder contains TensorFlow")
 
 if(TensorFlow_DIR STREQUAL "")
   unset(TensorFlow_DIR CACHE)
-  find_path(TensorFlow_DIR NAMES tensorflow PATHS /opt/tensorflow)
+  find_path(TensorFlow_DIR NAMES tensorflow PATHS /opt)
+  find_package_handle_standard_args(TensorFlow DEFAULT_MSG TensorFlow_DIR)
+  if(TENSORFLOW_FOUND)
+    message("aaaa ${TensorFlow_DIR}")
+    set(TensorFlow_DIR "${TensorFlow_DIR}/tensorflow")
+    set(TensorFlow_DIR "${TensorFlow_DIR}" CACHE PATH "Folder contains TensorFlow")
+    message("aaaa ${TensorFlow_DIR}")
+  endif()
 endif()
-
-find_path(TensorFlow_PB_INCLUDE_DIR NAMES tensorflow PATHS ${TensorFlow_DIR}/bazel-genfiles)
+message("aaaa ${TensorFlow_DIR}")
 
 find_library(TensorFlow_LIBRARY NAMES tensorflow_cc PATHS ${TensorFlow_DIR}/bazel-bin/tensorflow)
 
 EXECUTE_PROCESS(COMMAND bazel info output_base WORKING_DIRECTORY ${TensorFlow_DIR} OUTPUT_VARIABLE OUTPUT_BASE OUTPUT_STRIP_TRAILING_WHITESPACE)
 
+set(TensorFlow_PB_INCLUDE_DIR "${TensorFlow_DIR}/bazel-genfiles/tensorflow")
 set(TensorFlow_INCLUDE_DIR ${TensorFlow_PB_INCLUDE_DIR} ${TensorFlow_DIR} ${OUTPUT_BASE}/external/eigen_archive)
+message("aaaa ${TensorFlow_INCLUDE_DIR}")
 
 find_package_handle_standard_args(TensorFlow DEFAULT_MSG TensorFlow_INCLUDE_DIR TensorFlow_LIBRARY)
+message("aaaa ${TensorFlow_INCLUDE_DIR}")
+message("aaaaaaaaaaaa ${TensorFlow_LIBRARY}  ")
 
 if(TENSORFLOW_FOUND)
     set(TensorFlow_INCLUDE_DIRS ${TensorFlow_INCLUDE_DIR})
