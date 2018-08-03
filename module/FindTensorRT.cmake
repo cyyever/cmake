eda7ab26294aa65d16749c16506e9ad74cf83038
cyy <cyyever@outlook.com>
2018-08-03 15:16:58 +0800

    improve find code

--- module/FindTensorRT.cmake
+++ module/FindTensorRT.cmake
@@ -12,6 +12,17 @@ include(FindPackageHandleStandardArgs)
 
 set(TensorRT_DIR "" CACHE PATH "Folder contains TensorRT")
 
+if(TensorRT_DIR STREQUAL "")
+  unset(TensorRT_DIR CACHE)
+  find_path(TensorRT_DIR NAMES tensorRT PATHS /opt)
+  find_package_handle_standard_args(TensorRT DEFAULT_MSG TensorRT_DIR)
+  if(TENSORRT_FOUND)
+    set(TensorRT_DIR "${TensorRT_DIR}/tensorRT")
+    set(TensorRT_DIR "${TensorRT_DIR}" CACHE PATH "Folder contains TensorRT")
+  endif()
+endif()
+
+
 find_library(nvcaffe_parser_LIBRARY NAMES nvcaffe_parser PATHS ${TensorRT_DIR}/lib)
 find_library(nvinfer_LIBRARY NAMES nvinfer PATHS ${TensorRT_DIR}/lib)
 find_library(nvinfer_plugin_LIBRARY NAMES nvinfer_plugin PATHS ${TensorRT_DIR}/lib)
