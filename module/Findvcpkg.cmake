cae3186ad5c34fabd1e694dc8a7d96ce0e3424e5
cyy <cyyever@outlook.com>
2018-11-23 10:05:28 +0800

    fix vcpkg dir

--- module/Findvcpkg.cmake
+++ module/Findvcpkg.cmake
@@ -5,10 +5,15 @@
 #  vcpkg_dir
 #  vcpkg_toolchain_file
 
-include(FindPackageHandleStandardArgs)
-find_file(vcpkg_dir vcpkg PATHS c:/code ~)
-find_package_handle_standard_args(vcpkg DEFAULT_MSG vcpkg_dir)
-if(vcpkg_FOUND)
-  find_file(vcpkg_toolchain_file vcpkg.cmake PATHS ${vcpkg_dir}/scripts/buildsystems)
-  find_package_handle_standard_args(vcpkg DEFAULT_MSG vcpkg_dir vcpkg_toolchain_file)
+if(NOT DEFINED vcpkg_dir)
+  include(FindPackageHandleStandardArgs)
+  find_file(vcpkg_dir vcpkg PATHS c:/code ~)
+  find_package_handle_standard_args(vcpkg DEFAULT_MSG vcpkg_dir)
+  if(NOT vcpkg_FOUND)
+    return()
+  endif()
+else()
+  file(TO_CMAKE_PATH "${vcpkg_dir}" vcpkg_dir)
 endif()
+find_file(vcpkg_toolchain_file vcpkg.cmake PATHS ${vcpkg_dir}/scripts/buildsystems)
+find_package_handle_standard_args(vcpkg DEFAULT_MSG vcpkg_dir vcpkg_toolchain_file)
