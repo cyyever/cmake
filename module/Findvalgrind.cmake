f51f51cd8f4495029aae640057c594199f8bdafb
cyy <cyyever@outlook.com>
2018-12-18 14:28:48 +0800

    improve module

--- module/Findvalgrind.cmake
+++ module/Findvalgrind.cmake
@@ -2,9 +2,13 @@
 #
 # The following are set after configuration is done:
 #  valgrind_FOUND
-#  valgrind_BINARY
+#  valgrind::valgrind
 
 include_guard()
 include(FindPackageHandleStandardArgs)
-find_program(valgrind_BINARY valgrind PATHS /usr/bin /usr/local/bin)
+find_program(valgrind_BINARY NAMES valgrind PATH_SUFFIXES "valgrind")
 find_package_handle_standard_args(valgrind DEFAULT_MSG valgrind_BINARY)
+if(valgrind_FOUND AND NOT TARGET valgrind::valgrind)
+  add_executable(valgrind::valgrind IMPORTED)
+  set_property(TARGET valgrind::valgrind PROPERTY IMPORTED_LOCATION "${valgrind_BINARY}")
+endif()
