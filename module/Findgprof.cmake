8ea8499df5debd1a8845c4e65c2cd8ed8667647f
cyy <cyyever@outlook.com>
2018-12-18 11:34:36 +0800

    improve modules

--- module/Findgprof.cmake
+++ module/Findgprof.cmake
@@ -2,9 +2,13 @@
 #
 # The following are set after configuration is done:
 #  gprof_FOUND
-#  gprof_BINARY
+#  gprof::gprof
 
 include_guard()
 include(FindPackageHandleStandardArgs)
-find_program(gprof_BINARY gprof PATHS /usr/bin /usr/local/bin)
+find_program(gprof_BINARY NAMES gprof)
 find_package_handle_standard_args(gprof DEFAULT_MSG gprof_BINARY)
+if(gprof_FOUND AND NOT TARGET gprof::gprof)
+  add_executable(gprof::gprof IMPORTED)
+  set_property(TARGET gprof::gprof PROPERTY IMPORTED_LOCATION "${gprof_BINARY}")
+endif()
