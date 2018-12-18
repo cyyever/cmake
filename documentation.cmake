eb346f8240c70fd7d5366236673b3cb2cc067c63
cyy <cyyever@outlook.com>
2018-12-18 17:49:33 +0800

    improve code

--- documentation.cmake
+++ documentation.cmake
@@ -1,11 +1,12 @@
+include_guard()
 find_package(Doxygen)
 
-if(DOXYGEN_FOUND AND NOT TARGET generate_document)
-  add_custom_target(generate_document 
+if(DOXYGEN_FOUND AND NOT TARGET generate-document)
+  add_custom_target(generate-document
     COMMAND Doxygen::doxygen -g
     COMMAND sed -i -e "s@My Project@${CMAKE_PROJECT_NAME}@" Doxyfile
     COMMAND sed -i -e "s@^[[:space:]]*INPUT[[:space:]]*=.*@INPUT = ${CMAKE_SOURCE_DIR}/src@" Doxyfile
-    COMMAND sed -i -e "s@^[[:space:]]*OUTPUT_DIRECTORY[[:space:]]*=.*@OUTPUT_DIRECTORY = ${CMAKE_BINARY_DIR}/doc@" Doxyfile
+    COMMAND sed -i -e "s@^[[:space:]]*OUTPUT_DIRECTORY[[:space:]]*=.*@OUTPUT_DIRECTORY = ${CMAKE_BINARY_DIR}/documentation@" Doxyfile
     COMMAND sed -i -e "s@^[[:space:]]*GENERATE_LATEX[[:space:]]*=.*@GENERATE_LATEX = NO@" Doxyfile
     COMMAND Doxygen::doxygen)
 endif()
