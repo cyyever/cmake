58f63231ccfb61c4dee5092b5b53692625029b28
cyy <cyyever@outlook.com>
2018-12-18 17:04:57 +0800

    improve module

--- documentation.cmake
+++ documentation.cmake
@@ -1,7 +1,8 @@
-FIND_PACKAGE(Doxygen)
+find_package(Doxygen)
 
-IF(DOXYGEN_FOUND AND NOT TARGET doc)
-  ADD_CUSTOM_TARGET(doc COMMAND Doxygen::doxygen -g
+if(DOXYGEN_FOUND AND NOT TARGET generate_document)
+  add_custom_target(generate_document 
+    COMMAND Doxygen::doxygen -g
     COMMAND sed -i -e "s@My Project@${CMAKE_PROJECT_NAME}@" Doxyfile
     COMMAND sed -i -e "s@^[[:space:]]*INPUT[[:space:]]*=.*@INPUT = ${CMAKE_SOURCE_DIR}/src@" Doxyfile
     COMMAND sed -i -e "s@^[[:space:]]*OUTPUT_DIRECTORY[[:space:]]*=.*@OUTPUT_DIRECTORY = ${CMAKE_BINARY_DIR}/doc@" Doxyfile
