88c6214a06ffdddc9caad35e545121e868407da1
cyy <cyyever@outlook.com>
2019-02-07 09:30:29 +0800

    improve script

--- static_code_analysis.cmake
+++ static_code_analysis.cmake
@@ -22,11 +22,11 @@ set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR}/module)
 
 find_package(ClangTools QUIET)
-if(clang-tidy_FOUND AND run-clang-tidy_FOUND AND NOT TARGET do-run-clang-tidy)
+if(clang-tidy_FOUND AND run-clang-tidy_FOUND AND NOT TARGET do_run_clang_tidy)
   set(EXTRA-ARGS -extra-arg='-std=c++2a' -extra-arg='-Qunused-arguments')
   set(CHECKES "-checks='*,-fuchsia-default-arguments,-clang-analyzer-cplusplus.NewDeleteLeaks,-clang-diagnostic-ignored-optimization-argument,-readability-implicit-bool-conversion,-llvm-namespace-comment,-google-readability-namespace-comments,-cppcoreguidelines-owning-memory,-cert-err58-cpp,-fuchsia-statically-constructed-objects,-clang-diagnostic-gnu-zero-variadic-macro-arguments,-cppcoreguidelines-pro-bounds-pointer-arithmetic,-cppcoreguidelines-pro-type-vararg,-cppcoreguidelines-avoid-magic-numbers,-hicpp-vararg,-readability-magic-numbers,-cppcoreguidelines-pro-bounds-array-to-pointer-decay,-hicpp-no-array-decay'")
     add_custom_target(
-      do-run-clang-tidy
+      do_run_clang_tidy
       COMMAND ClangTools::run-clang-tidy -clang-tidy-binary "$<TARGET_FILE:ClangTools::clang-tidy>" -p ${CMAKE_BINARY_DIR} "-quiet" ${EXTRA-ARGS} ${CHECKES} > ./run-clang-tidy.txt
       DEPENDS ${CMAKE_BINARY_DIR}/compile_commands.json
       WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
@@ -34,18 +34,18 @@ if(clang-tidy_FOUND AND run-clang-tidy_FOUND AND NOT TARGET do-run-clang-tidy)
 endif()
 
 find_package(cppcheck QUIET)
-if(cppcheck_FOUND AND NOT TARGET do-cppcheck) 
-  add_custom_target(do-cppcheck
-    COMMAND cppcheck::cppcheck --project=${CMAKE_BINARY_DIR}/compile_commands.json --std=c++14 --enable=all --inconclusive 2> ./do-cppcheck.txt
+if(cppcheck_FOUND AND NOT TARGET do_cppcheck)
+  add_custom_target(do_cppcheck
+    COMMAND cppcheck::cppcheck --project=${CMAKE_BINARY_DIR}/compile_commands.json --std=c++14 --enable=all --inconclusive 2> ./do_cppcheck.txt
     DEPENDS ${CMAKE_BINARY_DIR}/compile_commands.json
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
     )
 endif()
 
 find_package(iwyu QUIET)
-if(iwyu_tool_FOUND AND NOT TARGET do-include-what-you-use)
-  add_custom_target(do-include-what-you-use
-    COMMAND iwyu::iwyu_tool -p ${CMAKE_BINARY_DIR} -- --transitive_includes_only > ./do-include-what-you-use.txt
+if(iwyu_tool_FOUND AND NOT TARGET do_include_what_you_use)
+  add_custom_target(do_include_what_you_use
+    COMMAND iwyu::iwyu_tool -p ${CMAKE_BINARY_DIR} -- --transitive_includes_only > ./do_include_what_you_use.txt
     DEPENDS ${CMAKE_BINARY_DIR}/compile_commands.json
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
     )
@@ -56,7 +56,7 @@ if(WITH_PVSSTUDIO)
   find_package(PVS-Studio QUIET)
   if(PVS-Studio_FOUND)
     if(NOT WIN32)
-      add_custom_target(do-pvs-studio-analysis
+      add_custom_target(do_pvs_studio_analysis
         COMMAND grep '"file":' ${CMAKE_BINARY_DIR}/compile_commands.json | sed -e 's/"file"://' | xargs -I source_file sed -i -e '1i // This is an open source non-commercial project. Dear PVS-Studio, please check it.' -e '1i // PVS-Studio Static Code Analyzer for C, C++ and C\#: http://www.viva64.com' source_file
         COMMAND PVS-Studio::analyzer analyze -a 31 -o ./pvs-studio.log -j8 || true
         COMMAND PVS-Studio::plog-converter -t tasklist -a 'GA:1,2,3;64:1,2,3;OP:1,2,3;CS:1,2,3' -o ./pvs-studio-report.txt ./pvs-studio.log
@@ -67,7 +67,7 @@ if(WITH_PVSSTUDIO)
     else()
       find_package(powershell QUIET)
       if(powershell_FOUND) 
-        add_custom_target(do-pvs-studio-analysis
+        add_custom_target(do_pvs_studio_analysis
           COMMAND PVS-Studio::Cmd --incremental ScanAndAnalyze --target ${CMAKE_BINARY_DIR}/${CMAKE_PROJECT_NAME}.sln --progress -o ./pvs-studio.log
           COMMAND PVS-Studio::plog-converter -t FullHtml,Tasks -o . -n pvs-studio-report ./pvs-studio.log 
           COMMAND powershell::powershell rm ./pvs-studio.log
