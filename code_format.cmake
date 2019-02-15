72fca0368a1aedc162622632340c32a061f5234c
cyy <cyyever@outlook.com>
2019-02-15 15:02:03 +0800

    add header format

--- code_format.cmake
+++ code_format.cmake
@@ -8,7 +8,8 @@ find_package(ClangFormat QUIET)
 if(ClangFormat_FOUND AND NOT TARGET do_clang_format)
     add_custom_target(
       do_clang_format
-      COMMAND grep '"file":' ${CMAKE_BINARY_DIR}/compile_commands.json | sed -e 's/"file"://' | xargs -I source_file $<TARGET_FILE:ClangFormat::clang-format> -style=file -i source_file
+      COMMAND grep '"file":' ${CMAKE_BINARY_DIR}/compile_commands.json | sed -e 's/"file"://' | xargs -I source_file "$<TARGET_FILE:ClangFormat::clang-format>" -style=file -i source_file
+      COMMAND grep '"file":' ${CMAKE_BINARY_DIR}/compile_commands.json | sed -e 's/"file"://' | xargs -I source_file dirname source_file | sort | uniq | xargs -I dir find dir -name '*.hpp' | xargs -I source_file "$<TARGET_FILE:ClangFormat::clang-format>" -style=file -i source_file
       DEPENDS ${CMAKE_BINARY_DIR}/compile_commands.json
       WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
       )
