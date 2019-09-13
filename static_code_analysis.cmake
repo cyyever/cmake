634f44faaacbe5505ff9c8508fe6d578eff49914
cyy <cyyever@outlook.com>
2019-09-13 09:51:06 +0800

    fix pvs command line

--- static_code_analysis.cmake
+++ static_code_analysis.cmake
@@ -51,13 +51,13 @@ endif()
 
 find_package(cppcheck QUIET)
 if(NOT CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
-if(cppcheck_FOUND AND NOT TARGET do_cppcheck)
-  add_custom_target(do_cppcheck
-    COMMAND cppcheck::cppcheck --project=${CMAKE_BINARY_DIR}/compile_commands.json --std=c++14 --enable=all --inconclusive 2> ./do_cppcheck.txt
-    DEPENDS ${CMAKE_BINARY_DIR}/compile_commands.json
-    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-    )
-endif()
+  if(cppcheck_FOUND AND NOT TARGET do_cppcheck)
+    add_custom_target(do_cppcheck
+      COMMAND cppcheck::cppcheck --project=${CMAKE_BINARY_DIR}/compile_commands.json --std=c++14 --enable=all --inconclusive 2> ./do_cppcheck.txt
+      DEPENDS ${CMAKE_BINARY_DIR}/compile_commands.json
+      WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+      )
+  endif()
 endif()
 
 find_package(iwyu QUIET)
@@ -71,30 +71,26 @@ if(NOT CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
   endif()
 endif()
 
-option(WITH_PVSSTUDIO "use PVS-Studio for static analysis" OFF)
-if(WITH_PVSSTUDIO)
-  find_package(PVS-Studio QUIET)
-  if(PVS-Studio_FOUND)
-    if(NOT WIN32)
+find_package(PVS-Studio QUIET)
+if(PVS-Studio_FOUND)
+  if(NOT WIN32)
+    add_custom_target(do_pvs_studio_analysis
+      COMMAND PVS-Studio::analyzer analyze --incremental -a 31 -o ./pvs-studio.log -j8 || true
+      COMMAND PVS-Studio::plog-converter -t tasklist -a 'GA:1,2,3;64:1,2,3;OP:1,2,3;CS:1,2,3' -o ./pvs-studio-report.txt ./pvs-studio.log
+      COMMAND rm ./pvs-studio.log
+      DEPENDS ${CMAKE_BINARY_DIR}/compile_commands.json
+      WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+      )
+  else()
+    find_package(powershell QUIET)
+    if(powershell_FOUND)
       add_custom_target(do_pvs_studio_analysis
-        COMMAND grep '"file":' ${CMAKE_BINARY_DIR}/compile_commands.json | sed -e 's/"file"://' | xargs -I source_file sed -i -e '1i // This is an open source non-commercial project. Dear PVS-Studio, please check it.' -e '1i // PVS-Studio Static Code Analyzer for C, C++ and C\#: http://www.viva64.com' source_file
-        COMMAND PVS-Studio::analyzer analyze -a 31 -o ./pvs-studio.log -j8 || true
-        COMMAND PVS-Studio::plog-converter -t tasklist -a 'GA:1,2,3;64:1,2,3;OP:1,2,3;CS:1,2,3' -o ./pvs-studio-report.txt ./pvs-studio.log
-        COMMAND rm ./pvs-studio.log
-        DEPENDS ${CMAKE_BINARY_DIR}/compile_commands.json
+        COMMAND PVS-Studio::Cmd --incremental ScanAndAnalyze --target ${CMAKE_BINARY_DIR}/${CMAKE_PROJECT_NAME}.sln --progress -o ./pvs-studio.log
+        COMMAND PVS-Studio::plog-converter -t FullHtml,Tasks -o . -n pvs-studio-report ./pvs-studio.log
+        COMMAND powershell::powershell rm ./pvs-studio.log
+        DEPENDS ${CMAKE_BINARY_DIR}/${CMAKE_PROJECT_NAME}.sln
         WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
         )
-    else()
-      find_package(powershell QUIET)
-      if(powershell_FOUND)
-        add_custom_target(do_pvs_studio_analysis
-          COMMAND PVS-Studio::Cmd --incremental ScanAndAnalyze --target ${CMAKE_BINARY_DIR}/${CMAKE_PROJECT_NAME}.sln --progress -o ./pvs-studio.log
-          COMMAND PVS-Studio::plog-converter -t FullHtml,Tasks -o . -n pvs-studio-report ./pvs-studio.log
-          COMMAND powershell::powershell rm ./pvs-studio.log
-          DEPENDS ${CMAKE_BINARY_DIR}/${CMAKE_PROJECT_NAME}.sln
-          WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-          )
-      endif()
     endif()
   endif()
 endif()
