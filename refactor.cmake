c12bc6082d2037edc445508769ee7ee38d122473
cyy <cyyever@outlook.com>
2020-02-02 15:28:49 +0800

    fix code

--- refactor.cmake
+++ refactor.cmake
@@ -6,7 +6,7 @@ list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR}/module)
 
 find_package(ClangTools QUIET)
 if(clang-tidy_FOUND AND run-clang-tidy_FOUND AND clang-apply-replacements_FOUND AND NOT TARGET do_clang_tidy_fix)
-    set(CHECKES "-checks='-*,modernize*'")
+    set(CHECKES "-checks='-*,modernize*,-modernize-use-trailing-return-type'")
     add_custom_target(
       do_clang_tidy_fix
       COMMAND ClangTools::run-clang-tidy -fix -clang-tidy-binary "$<TARGET_FILE:ClangTools::clang-tidy>" -clang-apply-replacements-binary "$<TARGET_FILE:ClangTools::clang-apply-replacements>" -p ${CMAKE_BINARY_DIR} "-quiet" ${CHECKES}
