630ff6b41ab68ae57aa8995d7803a90d74656ebb
cyy <cyyever@outlook.com>
2020-03-26 14:31:43 +0800

    fix pvs command line

--- static_code_analysis.cmake
+++ static_code_analysis.cmake
@@ -87,7 +87,7 @@ if(PVS-Studio_FOUND)
   if(NOT WIN32)
     add_custom_target(
       do_pvs_studio_analysis
-      COMMAND PVS-Studio::analyzer analyze --incremental -a 31 -o
+      COMMAND PVS-Studio::analyzer analyze -a 31 -o
               ./pvs-studio.log -j8 || true
       COMMAND
         PVS-Studio::plog-converter -t tasklist -a
