170a8e3fe824af89326074890e517e61a8e15826
cyy <cyyever@outlook.com>
2020-04-03 16:52:09 +0800

    add partial code

--- module/FindGoogleSanitizer.cmake
+++ module/FindGoogleSanitizer.cmake
@@ -33,18 +33,32 @@ foreach(sanitizer_name IN ITEMS address thread undefined leak memory)
     continue()
   endif()
 
-  set(CMAKE_REQUIRED_FLAGS "-fsanitize=${sanitizer_name}")
+  if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC" OR CMAKE_C_COMPILER_ID STREQUAL "MSVC")
+    if(sanitizer_name STREQUAL "address")
+      set(CMAKE_REQUIRED_FLAGS "/fsanitize=address")
+      # set(CMAKE_REQUIRED_LIBRARIES "C:/Program Files/LLVM/lib/clang/10.0.0/lib/windows/clang_rt.asan_dynamic-x86_64.lib")
+      set(CMAKE_REQUIRED_LIBRARIES 
+        "C:/Program Files/LLVM/lib/clang/10.0.0/lib/windows/clang_rt.asan_cxx-x86_64.lib"
+        # "C:/Program Files/LLVM/lib/clang/10.0.0/lib/windows/clang_rt.asan-x86_64.lib"
+        )
+    else()
+      set(${sanitizer_name}_sanitizer_FOUND FALSE)
+      continue()
+    endif()
+  else()
+    set(CMAKE_REQUIRED_FLAGS "-fsanitize=${sanitizer_name}" -fno-omit-frame-pointer)
+  endif()
 
   set(CMAKE_REQUIRED_QUIET ON)
   set(_run_res 0)
-  if("CXX" IN_LIST languages AND NOT CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+  if("CXX" IN_LIST languages)
     include(CheckCXXSourceRuns)
     check_cxx_source_runs("${_source_code}" __cxx_${sanitizer_name}_res)
     if(__cxx_${sanitizer_name}_res)
       set(_run_res 1)
     endif()
   endif()
-  if("C" IN_LIST languages AND NOT CMAKE_C_COMPILER_ID STREQUAL "MSVC")
+  if("C" IN_LIST languages)
     include(CheckCSourceRuns)
     check_c_source_runs("${_source_code}" __c_${sanitizer_name}_res)
     if(__c_${sanitizer_name}_res)
@@ -62,13 +76,14 @@ foreach(sanitizer_name IN ITEMS address thread undefined leak memory)
       INTERFACE
         $<$<AND:$<COMPILE_LANGUAGE:CXX>,$<BOOL:$__cxx_${sanitizer_name}_res>>:${CMAKE_REQUIRED_FLAGS}>
         $<$<AND:$<COMPILE_LANGUAGE:C>,$<BOOL:$__c_${sanitizer_name}_res>>:${CMAKE_REQUIRED_FLAGS}>
-        -fno-omit-frame-pointer)
-    target_link_options(
-      GoogleSanitizer::${sanitizer_name}
-      INTERFACE
-      $<$<AND:$<COMPILE_LANGUAGE:CXX>,$<BOOL:$__cxx_${sanitizer_name}_res>>:${CMAKE_REQUIRED_FLAGS}>
-      $<$<AND:$<COMPILE_LANGUAGE:C>,$<BOOL:$__c_${sanitizer_name}_res>>:${CMAKE_REQUIRED_FLAGS}>
-      -fno-omit-frame-pointer)
+        )
+    if(NOT CMAKE_CXX_COMPILER_ID STREQUAL "MSVC" AND NOT CMAKE_C_COMPILER_ID STREQUAL "MSVC")
+      target_link_options(
+        GoogleSanitizer::${sanitizer_name}
+        INTERFACE
+        $<$<AND:$<COMPILE_LANGUAGE:CXX>,$<BOOL:$__cxx_${sanitizer_name}_res>>:${CMAKE_REQUIRED_FLAGS}>
+        $<$<AND:$<COMPILE_LANGUAGE:C>,$<BOOL:$__c_${sanitizer_name}_res>>:${CMAKE_REQUIRED_FLAGS}>)
+    endif()
 
     if(sanitizer_name STREQUAL "address")
       target_link_options(
@@ -77,6 +92,10 @@ foreach(sanitizer_name IN ITEMS address thread undefined leak memory)
         $<$<AND:$<COMPILE_LANGUAGE:CXX>,$<BOOL:$__cxx_${sanitizer_name}_res>,$<CXX_COMPILER_ID:GNU>>:-lasan>
         $<$<AND:$<COMPILE_LANGUAGE:C>,$<BOOL:$__c_${sanitizer_name}_res>,$<C_COMPILER_ID:GNU>>:-lasan>
       )
+      if(__cxx_${sanitizer_name}_res OR __c_${sanitizer_name}_res)
+        message("aaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+        target_link_libraries(GoogleSanitizer::${sanitizer_name} INTERFACE ${CMAKE_REQUIRED_LIBRARIES})
+      endif()
     endif()
     if(sanitizer_name STREQUAL "undefined")
       target_link_options(
