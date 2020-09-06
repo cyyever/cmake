b4dc31beb702ba8cf043dcef0d0147a75a629c34
cyy <cyyever@outlook.com>
2020-09-06 14:58:14 +0800

    fix path

--- module/FindClangTools.cmake
+++ module/FindClangTools.cmake
@@ -13,17 +13,28 @@ include(FindPackageHandleStandardArgs)
 
 set(CMAKE_SUPPRESS_DEVELOPER_WARNINGS ON)
 foreach(program_name IN ITEMS clang-tidy clang-apply-replacements)
-  find_program(${program_name}_BINARY NAMES ${program_name}-devel ${program_name}-10 ${program_name}-9 ${program_name}-8 ${program_name} PATH_SUFFIXES "LLVM/bin")
-  find_package_handle_standard_args(${program_name} DEFAULT_MSG ${program_name}_BINARY)
+  find_program(
+    ${program_name}_BINARY
+    NAMES ${program_name}-devel ${program_name} ${program_name}-10
+    PATH_SUFFIXES "llvm/master/bin" "LLVM/bin")
+  find_package_handle_standard_args(${program_name} DEFAULT_MSG
+                                    ${program_name}_BINARY)
   if(${program_name}_FOUND AND NOT TARGET ClangTools::${program_name})
     add_executable(ClangTools::${program_name} IMPORTED)
-    set_property(TARGET ClangTools::${program_name} PROPERTY IMPORTED_LOCATION "${${program_name}_BINARY}")
+    set_property(TARGET ClangTools::${program_name}
+                 PROPERTY IMPORTED_LOCATION "${${program_name}_BINARY}")
   endif()
 endforeach()
 
-find_program(run-clang-tidy_BINARY NAMES run-clang-tidy run-clang-tidy.py PATH_SUFFIXES "LLVM/bin" "llvm-devel/share/clang" "share/clang")
-find_package_handle_standard_args(run-clang-tidy DEFAULT_MSG run-clang-tidy_BINARY)
+find_program(
+  run-clang-tidy_BINARY
+  NAMES run-clang-tidy run-clang-tidy.py
+  PATH_SUFFIXES "llvm/master/share/clang" "LLVM/bin" "llvm-devel/share/clang"
+                "share/clang")
+find_package_handle_standard_args(run-clang-tidy DEFAULT_MSG
+                                  run-clang-tidy_BINARY)
 if(run-clang-tidy_FOUND AND NOT TARGET ClangTools::run-clang-tidy)
   add_executable(ClangTools::run-clang-tidy IMPORTED)
-  set_property(TARGET ClangTools::run-clang-tidy PROPERTY IMPORTED_LOCATION "${run-clang-tidy_BINARY}")
+  set_property(TARGET ClangTools::run-clang-tidy
+               PROPERTY IMPORTED_LOCATION "${run-clang-tidy_BINARY}")
 endif()
