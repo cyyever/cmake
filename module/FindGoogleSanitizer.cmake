8ed5d8eb85af51db4306c02c8a42f0810faa9336
cyy <cyyever@outlook.com>
2021-03-19 13:52:06 +0800

    fix code

--- module/FindGoogleSanitizer.cmake
+++ module/FindGoogleSanitizer.cmake
@@ -40,59 +40,48 @@ foreach(sanitizer_name IN ITEMS address thread undefined leak memory)
 
   set(CMAKE_REQUIRED_QUIET ON)
   set(_run_res 0)
-  if("CXX" IN_LIST languages)
-    include(CheckCXXSourceRuns)
-    check_cxx_source_runs("${_source_code}" __cxx_${sanitizer_name}_res)
-    if(__cxx_${sanitizer_name}_res)
+  include(CheckSourceRuns)
+  foreach(lang IN LISTS languages)
+    check_source_runs(${lang} "${_source_code}" __${lang}_${sanitizer_name}_res)
+    if(__${lang}_${sanitizer_name}_res)
       set(_run_res 1)
     endif()
-  endif()
-  if("C" IN_LIST languages)
-    include(CheckCSourceRuns)
-    check_c_source_runs("${_source_code}" __c_${sanitizer_name}_res)
-    if(__c_${sanitizer_name}_res)
-      set(_run_res 1)
-    endif()
-  endif()
-
-  set(CMAKE_SUPPRESS_DEVELOPER_WARNINGS ON)
-  find_package_handle_standard_args(${sanitizer_name}_sanitizer DEFAULT_MSG
-                                    _run_res)
-  if(${sanitizer_name}_sanitizer_FOUND)
+  endforeach()
+  if(_run_res)
     add_library(GoogleSanitizer::${sanitizer_name} INTERFACE IMPORTED GLOBAL)
     target_compile_options(
       GoogleSanitizer::${sanitizer_name}
       INTERFACE
-        $<$<AND:$<COMPILE_LANGUAGE:CXX>,$<BOOL:$__cxx_${sanitizer_name}_res>>:${CMAKE_REQUIRED_FLAGS}>
-        $<$<AND:$<COMPILE_LANGUAGE:C>,$<BOOL:$__c_${sanitizer_name}_res>>:${CMAKE_REQUIRED_FLAGS}>
+        $<$<AND:$<COMPILE_LANGUAGE:CXX>,$<BOOL:$__CXX_${sanitizer_name}_res>>:${CMAKE_REQUIRED_FLAGS}>
+        $<$<AND:$<COMPILE_LANGUAGE:C>,$<BOOL:$__C_${sanitizer_name}_res>>:${CMAKE_REQUIRED_FLAGS}>
     )
     target_link_options(
       GoogleSanitizer::${sanitizer_name}
       INTERFACE
-      $<$<AND:$<COMPILE_LANGUAGE:CXX>,$<BOOL:$__cxx_${sanitizer_name}_res>>:${CMAKE_REQUIRED_FLAGS}>
-      $<$<AND:$<COMPILE_LANGUAGE:C>,$<BOOL:$__c_${sanitizer_name}_res>>:${CMAKE_REQUIRED_FLAGS}>
+      $<$<AND:$<COMPILE_LANGUAGE:CXX>,$<BOOL:$__CXX_${sanitizer_name}_res>>:${CMAKE_REQUIRED_FLAGS}>
+      $<$<AND:$<COMPILE_LANGUAGE:C>,$<BOOL:$__C_${sanitizer_name}_res>>:${CMAKE_REQUIRED_FLAGS}>
     )
 
     if(sanitizer_name STREQUAL "address")
       target_compile_definitions(
         GoogleSanitizer::${sanitizer_name}
         INTERFACE
-          $<$<AND:$<COMPILE_LANGUAGE:CXX>,$<BOOL:$__cxx_${sanitizer_name}_res>>:_GLIBCXX_SANITIZE_VECTOR>
-          $<$<AND:$<COMPILE_LANGUAGE:CXX>,$<BOOL:$__cxx_${sanitizer_name}_res>>:_GLIBCXX_SANITIZE_STD_ALLOCATOR>
+          $<$<AND:$<COMPILE_LANGUAGE:CXX>,$<BOOL:$__CXX_${sanitizer_name}_res>>:_GLIBCXX_SANITIZE_VECTOR>
+          $<$<AND:$<COMPILE_LANGUAGE:CXX>,$<BOOL:$__CXX_${sanitizer_name}_res>>:_GLIBCXX_SANITIZE_STD_ALLOCATOR>
       )
       target_link_options(
         GoogleSanitizer::${sanitizer_name}
         INTERFACE
-        $<$<AND:$<COMPILE_LANGUAGE:CXX>,$<BOOL:$__cxx_${sanitizer_name}_res>,$<CXX_COMPILER_ID:GNU>>:-lasan>
-        $<$<AND:$<COMPILE_LANGUAGE:C>,$<BOOL:$__c_${sanitizer_name}_res>,$<C_COMPILER_ID:GNU>>:-lasan>
+        $<$<AND:$<COMPILE_LANGUAGE:CXX>,$<BOOL:$__CXX_${sanitizer_name}_res>,$<CXX_COMPILER_ID:GNU>>:-lasan>
+        $<$<AND:$<COMPILE_LANGUAGE:C>,$<BOOL:$__C_${sanitizer_name}_res>,$<C_COMPILER_ID:GNU>>:-lasan>
       )
     endif()
     if(sanitizer_name STREQUAL "undefined")
       target_link_options(
         GoogleSanitizer::${sanitizer_name}
         INTERFACE
-        $<$<AND:$<COMPILE_LANGUAGE:CXX>,$<BOOL:$__cxx_${sanitizer_name}_res>,$<CXX_COMPILER_ID:GNU>>:-lubsan>
-        $<$<AND:$<COMPILE_LANGUAGE:C>,$<BOOL:$__c_${sanitizer_name}_res>,$<C_COMPILER_ID:GNU>>:-lubsan>
+        $<$<AND:$<COMPILE_LANGUAGE:CXX>,$<BOOL:$__CXX_${sanitizer_name}_res>,$<CXX_COMPILER_ID:GNU>>:-lubsan>
+        $<$<AND:$<COMPILE_LANGUAGE:C>,$<BOOL:$__C_${sanitizer_name}_res>,$<C_COMPILER_ID:GNU>>:-lubsan>
       )
     endif()
   endif()
