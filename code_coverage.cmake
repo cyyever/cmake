ff73dedfe423d0d05eec56ab50e8a111f55a7739
cyy <cyyever@outlook.com>
2024-10-11 11:29:52 +0800

    Fix code

--- code_coverage.cmake
+++ code_coverage.cmake
@@ -6,14 +6,18 @@ endif()
 include(${CMAKE_CURRENT_LIST_DIR}/build_type.cmake)
 
 if(PROJECT_IS_TOP_LEVEL AND CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
-  add_custom_build_type(coverage)
-  set(CMAKE_CXX_FLAGS_COVERAGE "-g -O0 --coverage")
+  add_custom_build_type_like(coverage debug)
+  set(CMAKE_CXX_FLAGS_COVERAGE "${CMAKE_CXX_FLAGS_COVERAGE} -g -O0 --coverage")
   if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-    string(APPEND CMAKE_CXX_FLAGS_COVERAGE " -fprofile-abs-path")
+    string(APPEND CMAKE_CXX_FLAGS_COVERAGE
+           "${CMAKE_CXX_FLAGS_COVERAGE} -fprofile-abs-path")
   endif()
-  set(CMAKE_EXE_LINKER_FLAGS_COVERAGE "--coverage")
-  set(CMAKE_SHARED_LINKER_FLAGS_COVERAGE "--coverage")
-  set(CMAKE_MODULE_LINKER_FLAGS_COVERAGE "--coverage")
+  set(CMAKE_EXE_LINKER_FLAGS_COVERAGE
+      "${CMAKE_EXE_LINKER_FLAGS_COVERAGE} --coverage")
+  set(CMAKE_SHARED_LINKER_FLAGS_COVERAGE
+      "${CMAKE_SHARED_LINKER_FLAGS_COVERAGE} --coverage")
+  set(CMAKE_MODULE_LINKER_FLAGS_COVERAGE
+      "${CMAKE_MODULE_LINKER_FLAGS_COVERAGE} --coverage")
 endif()
 
 add_custom_command(
@@ -30,16 +34,15 @@ add_custom_target(
     -DCMAKE_BUILD_PARALLEL_LEVEL=${CMAKE_BUILD_PARALLEL_LEVEL}
     ${CMAKE_SOURCE_DIR}
   COMMAND ${CMAKE_COMMAND} --build .
-  COMMAND ${CMAKE_CTEST_COMMAND} -C coverage
+  COMMAND ${CMAKE_CTEST_COMMAND} -T Test -T Coverage
   DEPENDS ${CMAKE_BINARY_DIR}
   WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
 
+# --gcov-object-directory ${CMAKE_BINARY_DIR}
 add_custom_target(
   generate_code_coverage_report
-  COMMAND mkdir -p code_coverage_report
-  COMMAND gcovr --root ${CMAKE_SOURCE_DIR} --html-details code_coverage_report/html -j
-          10 ${CMAKE_BINARY_DIR}
-  DEPENDS ${CMAKE_BINARY_DIR}/code_coverage
+  COMMAND
+    gcovr --root ${CMAKE_SOURCE_DIR} --html-details code_coverage_report.html
   WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
   BYPRODUCTS code_coverage_report)
 add_dependencies(generate_code_coverage_report do_test_for_code_coverage)
