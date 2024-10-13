71495b518e1baa2a8dfc3d46089198a0ca648a67
cyy <cyyever@outlook.com>
2024-10-13 08:08:28 +0800

    Improve sanitizer

--- module/FindSanitizer.cmake
+++ module/FindSanitizer.cmake
@@ -23,41 +23,45 @@ set(_source_code
   ]==])
 
 include(CMakePushCheckState)
-foreach(sanitizer_name IN ITEMS address thread undefined leak memory)
-  cmake_push_check_state(RESET)
-
-  set(CMAKE_REQUIRED_QUIET ON)
-  foreach(lang IN LISTS languages)
+foreach(lang IN LISTS languages)
+  if(lang STREQUAL C)
+    include(CheckCSourceCompiles)
+    include(CheckCSourceRuns)
+  elseif(lang STREQUAL CXX)
+    include(CheckCXXSourceCompiles)
+    include(CheckCXXSourceRuns)
+  else()
+    continue()
+  endif()
+  foreach(sanitizer_name IN ITEMS address thread undefined leak memory)
     if(TARGET Sanitizer::${sanitizer_name}_${lang})
       continue()
     endif()
-    if(NOT lang STREQUAL C AND NOT lang STREQUAL CXX)
-      continue()
-    endif()
+    cmake_push_check_state(RESET)
+    set(CMAKE_REQUIRED_QUIET ON)
     if(CMAKE_${lang}_COMPILER_ID STREQUAL "MSVC")
       if(sanitizer_name STREQUAL "address")
         set(SANITIZER_FLAGS "/fsanitize=${sanitizer_name}")
       else()
+        cmake_pop_check_state()
         continue()
       endif()
     else()
       set(SANITIZER_FLAGS
           "-fsanitize=${sanitizer_name};-fno-omit-frame-pointer")
-      if(CMAKE_${lang}_COMPILER_ID STREQUAL "Clang")
-        execute_process(
-          COMMAND ${CMAKE_${lang}_COMPILER} "--print-file-name"
-                  "libclang_rt.asan-x86_64.so"
-          ERROR_VARIABLE
-          error_variable
-          OUTPUT_VARIABLE
-          output_variable
-          OUTPUT_STRIP_TRAILING_WHITESPACE)
-        if(NOT error_variable)
-          if(output_variable)
-            cmake_path(GET output_variable PARENT_PATH asan_dir)
-            list(APPEND SANITIZER_FLAGS "-L${asan_dir}")
-          endif()
-        endif()
+    endif()
+    if(CMAKE_${lang}_COMPILER_ID STREQUAL "Clang")
+      unset(err_var)
+      unset(out_var)
+      execute_process(
+        COMMAND ${CMAKE_${lang}_COMPILER} "--print-file-name"
+                "libclang_rt.asan-x86_64.so"
+        ERROR_VARIABLE err_var
+        OUTPUT_VARIABLE out_var
+        OUTPUT_STRIP_TRAILING_WHITESPACE)
+      if(NOT err_var AND out_var)
+        cmake_path(GET out_var PARENT_PATH asan_dir)
+        list(APPEND SANITIZER_FLAGS "-L${asan_dir}")
       endif()
     endif()
     if(sanitizer_name STREQUAL "undefined" AND UBSAN_FLAGS)
@@ -76,46 +80,25 @@ foreach(sanitizer_name IN ITEMS address thread undefined leak memory)
       list(APPEND SANITIZER_LINK_FLAGS "/INCREMENTAL:NO")
     else()
       list(APPEND SANITIZER_LINK_FLAGS "-fsanitize=${sanitizer_name}")
-      if(CMAKE_${lang}_COMPILER_ID STREQUAL "Clang")
-        if(CMAKE_${lang}_COMPILER_ID STREQUAL "Clang")
-          execute_process(
-            COMMAND ${CMAKE_${lang}_COMPILER} "--print-file-name"
-                    "libclang_rt.asan-x86_64.so"
-            ERROR_VARIABLE
-            error_variable
-            OUTPUT_VARIABLE
-            output_variable
-            OUTPUT_STRIP_TRAILING_WHITESPACE)
-          if(NOT error_variable)
-            if(output_variable)
-              cmake_path(GET output_variable PARENT_PATH asan_dir)
-              list(APPEND SANITIZER_LINK_FLAGS "-L${asan_dir}")
-            endif()
-          endif()
-        endif()
-      endif()
     endif()
     set(CMAKE_REQUIRED_LINK_OPTIONS "${SANITIZER_LINK_FLAGS}")
 
     unset(__res)
     if(lang STREQUAL C)
       if(CMAKE_${lang}_COMPILER_ID STREQUAL "MSVC")
-        include(CheckCSourceCompiles)
         check_c_source_compiles("${_source_code}" __res)
       else()
-        include(CheckCSourceRuns)
         check_c_source_runs("${_source_code}" __res)
       endif()
     else()
       if(CMAKE_${lang}_COMPILER_ID STREQUAL "MSVC")
-        include(CheckCXXSourceCompiles)
         check_cxx_source_compiles("${_source_code}" __res)
       else()
-        include(CheckCXXSourceRuns)
         check_cxx_source_runs("${_source_code}" __res)
       endif()
     endif()
     if(NOT __res)
+      cmake_pop_check_state()
       continue()
     endif()
     add_library(Sanitizer::${sanitizer_name}_${lang} INTERFACE IMPORTED GLOBAL)
@@ -134,22 +117,20 @@ foreach(sanitizer_name IN ITEMS address thread undefined leak memory)
                           $<$<COMPILE_LANGUAGE:${lang}>:${SANITIZER_FLAG}>)
     endforeach()
 
-    if(sanitizer_name STREQUAL "address")
-      if(lang STREQUAL CXX)
-        if(CMAKE_${lang}_COMPILER_ID STREQUAL "MSVC")
-          target_compile_definitions(
-            Sanitizer::${sanitizer_name}_${lang}
-            INTERFACE $<$<COMPILE_LANGUAGE:${lang}>:_DISABLE_VECTOR_ANNOTATION>
-                      $<$<COMPILE_LANGUAGE:${lang}>:_DISABLE_STRING_ANNOTATION>)
-        else()
-          target_compile_definitions(
-            Sanitizer::${sanitizer_name}_${lang}
-            INTERFACE
-              $<$<COMPILE_LANGUAGE:${lang}>:_GLIBCXX_SANITIZE_VECTOR>
-              $<$<COMPILE_LANGUAGE:${lang}>:_GLIBCXX_SANITIZE_STD_ALLOCATOR>)
-        endif()
+    if(sanitizer_name STREQUAL "address" AND lang STREQUAL CXX)
+      if(CMAKE_${lang}_COMPILER_ID STREQUAL "MSVC")
+        target_compile_definitions(
+          Sanitizer::${sanitizer_name}_${lang}
+          INTERFACE $<$<COMPILE_LANGUAGE:${lang}>:_DISABLE_VECTOR_ANNOTATION>
+                    $<$<COMPILE_LANGUAGE:${lang}>:_DISABLE_STRING_ANNOTATION>)
+      else()
+        target_compile_definitions(
+          Sanitizer::${sanitizer_name}_${lang}
+          INTERFACE
+            $<$<COMPILE_LANGUAGE:${lang}>:_GLIBCXX_SANITIZE_VECTOR>
+            $<$<COMPILE_LANGUAGE:${lang}>:_GLIBCXX_SANITIZE_STD_ALLOCATOR>)
       endif()
     endif()
+    cmake_pop_check_state()
   endforeach()
-  cmake_pop_check_state()
 endforeach()
